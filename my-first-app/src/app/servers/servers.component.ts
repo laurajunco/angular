07b0c048a3eff40c05350b8a092f25b408e66e85
Laura Junco <laura@MacBook-Pro-de-Laura.local>
2022-03-26 20:21:47 +0100

    Combining all data binding

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,28 +1,28 @@
-import { Component, OnInit } from "@angular/core"
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-	selector: "app-servers",
-	templateUrl: "./servers.component.html",
-	styleUrls: ["./servers.component.css"]
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
-	allowNewServer = false
-	serverCreationStatus = "No server was created"
-	serverName = "test name"
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = 'test name';
 
-	constructor() {
-		setTimeout(() => {
-			this.allowNewServer = !this.allowNewServer
-		}, 2000)
-	}
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = !this.allowNewServer;
+    }, 2000);
+  }
 
-	ngOnInit(): void {}
+  ngOnInit(): void {}
 
-	onCreateServer() {
-		this.serverCreationStatus = "Server was created"
-	}
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created. Name :' + this.serverName;
+  }
 
-	onUpdateServerName(event: Event) {
-		this.serverName = (<HTMLInputElement> event.target).value
-	}
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
