18219abe3aeefb6dc863557f2175e4923cc9c6de
Laura Junco <laura@MacBook-Pro-de-Laura.local>
2022-03-31 10:41:41 +0200

    Using Components

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -9,10 +9,12 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
     RecipesComponent,
     RecipeListComponent,
     RecipeDetailComponent,
