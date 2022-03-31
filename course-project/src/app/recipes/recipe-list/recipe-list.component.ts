34d11c93b7c0032092042d727f03ede99246d899
Laura Junco <laura@MacBook-Pro-de-Laura.local>
2022-03-31 11:37:02 +0200

    Adding Content to the Recipes' Components

--- course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
-  
-  constructor() { }
+  recipes: Recipe[] = [
+    new Recipe(
+      'test Recipe',
+      'this is a test description',
+      'https://www.simplyrecipes.com/thmb/JWjdE8YwikAae0KZuyy6ZJW7Utw=/3000x2001/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1c-c2b1885d27d4481c9cfe6f6286a64342.jpg'
+    ),
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
