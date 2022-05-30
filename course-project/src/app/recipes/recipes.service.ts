af945f4b7e6772c7c32def435e57c96c158b4436
Laura Junco <l.juncogomez@celonis.com>
2022-05-30 10:54:08 +0200

    recipe: add ingredients to recipes

--- course-project/src/app/recipes/recipes.service.ts
+++ course-project/src/app/recipes/recipes.service.ts
@@ -5,14 +5,24 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
     new Recipe(
-      'Pizza',
-      'A delicious pizza recipe',
-      'https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg'
+      'Margherita Pizza',
+      'The best homemade margherita pizza with traditional dough',
+      'https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg',
+      [
+        { name: 'Flour', amount: 300 },
+        { name: 'Tomatoes', amount: 20 },
+        { name: 'Mozzarella cheese', amount: 150 },
+      ]
     ),
     new Recipe(
       'Stuffed Chicken fillet',
-      'Stuffed Chicken fillet for the whole family',
-      'https://upload.wikimedia.org/wikipedia/commons/5/57/990402-ians-recipe-01-IMG_4724.jpg'
+      'Stuffed chicken breasts make a great dinner party dish',
+      'https://upload.wikimedia.org/wikipedia/commons/5/57/990402-ians-recipe-01-IMG_4724.jpg',
+      [
+        { name: 'Chicken breast', amount: 2 },
+        { name: 'Mascarpone', amount: 3 },
+        { name: 'Prosciutto', amount: 4 },
+      ]
     ),
   ];
 
