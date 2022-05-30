e31c631fbe8bc3ed69fc764aafeeefae3a9609e0
Laura Junco <l.juncogomez@celonis.com>
2022-05-30 14:42:04 +0200

    Protecting Child (Nested) Routes Using the canActivateChild() Method

--- routing/src/app/app.module.ts
+++ routing/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
-import { RouterModule, Routes } from "@angular/router";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
 import { EditServerComponent } from "./servers/edit-server/edit-server.component";
@@ -11,26 +10,10 @@ import { ServersService } from "./servers/servers.service";
 import { UserComponent } from "./users/user/user.component";
 import { UsersComponent } from "./users/users.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { AuthGuard } from "./auth-guard.service";
+import { AuthService } from "./auth.service";
 
-const appRoutes: Routes = [
-  { path: "", component: HomeComponent }, //home page
-  {
-    path: "users",
-    component: UsersComponent,
-    children: [{ path: ":id/:name", component: UserComponent }],
-  },
-  {
-    path: "servers",
-    component: ServersComponent,
-    children: [
-      { path: ":id", component: ServerComponent },
-      { path: ":id/edit", component: EditServerComponent },
-    ],
-  },
-  { path: "not-found", component: PageNotFoundComponent },
-  // catch all paths you dont know, has to be the last route
-  { path: "**", redirectTo: "not-found" },
-];
 @NgModule({
   declarations: [
     AppComponent,
@@ -42,8 +25,8 @@ const appRoutes: Routes = [
     ServerComponent,
     PageNotFoundComponent,
   ],
-  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
-  providers: [ServersService],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  providers: [ServersService, AuthGuard, AuthService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
